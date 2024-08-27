b953d0e21ea8553167faca8837b0d80c36ca41b5
nzarhin <natan.zarhin@intel.com>
2024-08-27 15:20:38 +0300

    sdfsfs

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="word-counter",
-    version="0.1.1",
+    version="0.1.0",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
     install_requires=[],
